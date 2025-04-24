cba97e624b8481a02bd8e85dc37599058857499a
Andrew Sharaev <neon.robotics11@gmail.com>
2025-04-24 19:07:56 +0500

    tiktok, youtube, pinterest, tgBot

--- dowland video.py
+++ dowland video.py
@@ -1,17 +1,46 @@
-import yt_dlp
+import logging
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from telegram import ReplyKeyboardMarkup
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+async def pinterest(update, context):
+    await update.message.reply_text("скинь ссылку на изображение pinterest")
+
+async def youtube(update, context):
+    await update.message.reply_text("скинь ссылку на изображение youtube")
+
+async def tiktok(update, context):
+    await update.message.reply_text("скинь ссылку на изображение tiktok")
+
+async def start(update, context):
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Я умею скачивать видео по ссылке",
+        reply_markup=markup
+    )
+
+async def help_command(update, context):
+    await update.message.reply_text("Я пока не умею помогать...")
+
+def main():
+    application = Application.builder().token('7801940292:AAEEDCLRZO0f4vzTJyzEgOMNSpxQWwB-k3Q').build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    application.add_handler(CommandHandler("pinterest", pinterest))
+    application.add_handler(CommandHandler("youtube", youtube))
+    application.add_handler(CommandHandler("tiktok", tiktok))
 
 
-def download_tiktok_video_ytdlp(url, output_path="downloaded_video.mp4"):
-    ydl_opts = {
-        'outtmpl': output_path,  # Путь для сохранения
-        'format': 'best',  # Лучшее качество
-    }
+    application.run_polling()
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-        print(f"Видео скачано: {output_path}")
+reply_keyboard = [['/pinterest', '/youtube', '/tiktok']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
-
-# Пример использования
-tiktok_url = "https://www.tiktok.com/@username/video/1234567890"
-download_tiktok_video_ytdlp(tiktok_url)
+if __name__ == '__main__':
+    main()
